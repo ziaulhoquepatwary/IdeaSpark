a80282012f367406f97e483df5efa763dd2f2ac1
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-17 23:00:47 +0600

    Project setup done

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+
+function page() {
+    return (
+        <div className="p-8">
+            <h1 className="font-heading text-4xl font-bold">
+                New Project setup done
+            </h1>
+
+            <p className="mt-2 text-gray-600">
+                This is a normal paragraph text.
+            </p>
+        </div>
+    )
+}
+
+export default page
